cbcb5eb4697ba69f9836ddf2f6fb756c603dd468
Yücel Gül <yucellgull@hotmail.com>
2024-03-24 01:50:14 +0300

    Uygulama Anasayfa

---
+++ cifcidenSofraya/src/screens/HomeScreen/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import  {View, Text, Image} from "react-native"
+import styles from './styles';
+function Index() {
+  return (
+    <View>
+      <View style ={styles.headerMain}>
+        
+      </View>
+
+      <View>
+        
+      </View>
+    </View>
+  );
+}
+
+export default Index; 
---
+++ cifcidenSofraya/src/screens/HomeScreen/styles.ts
@@ -0,0 +1,13 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+const {height} = Dimensions.get('window')
+const styles = StyleSheet.create({
+    headerMain:{
+        height :30,
+        backgroundColor:'red',
+
+    } 
+
+})
+
+export default styles;
